239e8f31f53bbb30b9af09756ff89994a5187dd4
exepta <genshin.exepta@outlook.com>
2024-10-01 11:57:00 +0200

    feat: make the dev environment more interactive.

--- src/dev/dev_enemy.rs
+++ src/dev/dev_enemy.rs
@@ -11,6 +11,67 @@ pub struct TrapTower {
     pub(crate) bullet_offset: Vec3,
 }
 
+#[derive(Component, Clone, Copy, Debug)]
+pub enum TowerType {
+    Normal,
+    Laser,
+    Flamethrower,
+}
+
+impl TowerType {
+    fn get_tower(&self, materials: &mut ResMut<Assets<StandardMaterial>>) -> (Handle<StandardMaterial>, TrapTower) {
+        match self {
+            TowerType::Normal => (
+                materials.add(Color::srgb(0.5, 0.5, 0.8)),
+                TrapTower {
+                    shooting_timer: Timer::from_seconds(0.5, TimerMode::Repeating),
+                    bullet_offset: Vec3::new(0.0, 0.5, 0.0),
+                },
+            ),
+            TowerType::Laser => (
+                materials.add(Color::srgb(0.0, 0.5, 0.8)),
+                TrapTower {
+                    shooting_timer: Timer::from_seconds(0.001, TimerMode::Repeating),
+                    bullet_offset: Vec3::new(0.0, 0.5, 0.0),
+                },
+            ),
+            TowerType::Flamethrower => (
+                materials.add(Color::srgb(0.8, 0.0, 0.7)),
+                TrapTower {
+                    shooting_timer: Timer::from_seconds(0.1, TimerMode::Repeating),
+                    bullet_offset: Vec3::new(0.0, 0.5, 0.0),
+                },
+            ),
+        }
+    }
+
+    fn get_bullet(&self, direction: Vec3, materials: &mut ResMut<Assets<StandardMaterial>>) -> (Handle<StandardMaterial>, Bullet) {
+        match self {
+            TowerType::Normal => (
+                materials.add(Color::srgb(0.0, 0.5, 1.0)),
+                Bullet {
+                    direction,
+                    speed: 2.0,
+                }
+            ),
+            TowerType::Laser => (
+                materials.add(Color::srgb(0.0, 1.0, 0.4)),
+                Bullet {
+                    direction,
+                    speed: 10.0,
+                }
+            ),
+            TowerType::Flamethrower => (
+                materials.add(Color::srgb(1.0, 0.2, 0.0)),
+                Bullet {
+                    direction,
+                    speed: 1.5,
+                }
+            ),
+        }
+    }
+}
+
 pub struct BulletTowerPlugin;
 
 impl Plugin for BulletTowerPlugin {
@@ -23,10 +84,10 @@ impl Plugin for BulletTowerPlugin {
 pub fn update_tower_shooting(mut commands: Commands,
                              mut meshes: ResMut<Assets<Mesh>>,
                              mut materials: ResMut<Assets<StandardMaterial>>,
-                             mut towers: Query<(Entity, &mut TrapTower, &GlobalTransform)>,
+                             mut towers: Query<(Entity, &mut TrapTower, &TowerType, &GlobalTransform)>,
                              targets: Query<&GlobalTransform, With<Target>>,
                              time: Res<Time>) {
-    for (tower_en, mut tower, transform) in towers.iter_mut() {
+    for (tower_en, mut tower, tower_type, transform) in towers.iter_mut() {
         tower.shooting_timer.tick(time.delta());
         if tower.shooting_timer.just_finished() {
             let spawn_bullet = transform.translation() + tower.bullet_offset;
@@ -39,19 +100,19 @@ pub fn update_tower_shooting(mut commands: Commands,
                 .map(|nearest_target| nearest_target.translation() - spawn_bullet);
 
             if let Some(direction) = direction {
+                let (color, bullet) = tower_type.get_bullet(direction, &mut materials);
                 commands.entity(tower_en).with_children(|commands| {
                     //Bullet
                     commands.spawn(PbrBundle {
                         mesh: meshes.add(Cuboid::new(0.05, 0.05, 0.05)),
-                        material: materials.add(Color::srgb_u8(255, 0, 0)),
+                        material: color,
                         transform: Transform::from_translation(tower.bullet_offset),
                         ..default()
                     }).insert(Lifetime {
                         timer: Timer::from_seconds(3., TimerMode::Once),
-                    }).insert(Bullet {
-                        direction,
-                        speed: 8.0,
-                    }).insert(Name::new("Bullet"));
+                    })
+                        .insert(bullet)
+                        .insert(Name::new("Bullet"));
                 });
             }
         }
@@ -60,32 +121,41 @@ pub fn update_tower_shooting(mut commands: Commands,
 
 pub fn build_tower(mut commands: Commands,
                    selections: Query<(Entity, &PickSelection, &Transform)>,
+                   interaction: Query<(&Interaction, &TowerType), Changed<Interaction>>,
                    mut meshes: ResMut<Assets<Mesh>>,
-                   mut materials: ResMut<Assets<StandardMaterial>>,
-                   mouse: Res<ButtonInput<MouseButton>>) {
-    if mouse.just_pressed(MouseButton::Left) {
-        for (entity, selection, transform) in selections.iter() {
-            if selection.is_selected {
-                commands.entity(entity).despawn_recursive();
-                spawn_normal_tower(&mut commands, &mut meshes, &mut materials, transform.translation);
+                   mut materials: ResMut<Assets<StandardMaterial>>) {
+
+    for (interaction, tower_type) in interaction.iter() {
+        if matches!(interaction, Interaction::Pressed) {
+            for (entity, selection, transform) in selections.iter() {
+                if selection.is_selected {
+                    commands.entity(entity).despawn_recursive();
+                    spawn_tower(&mut commands, &mut meshes, &mut materials, transform.translation, *tower_type);
+                }
             }
         }
     }
 }
 
-pub fn spawn_normal_tower(commands: &mut Commands,
-                          meshes: &mut ResMut<Assets<Mesh>>,
-                          materials: &mut ResMut<Assets<StandardMaterial>>,
-                          position: Vec3) -> Entity {
-    commands.spawn(PbrBundle  {
-        mesh: meshes.add(Cuboid::new(0.5, 1.0, 0.5)),
-        material: materials.add(Color::srgb_u8(30, 30, 40)),
-        transform: Transform::from_translation(position),
-        ..default()
-    })
-        .insert(Name::new("TrapTower"))
-        .insert(TrapTower {
-            shooting_timer: Timer::from_seconds(0.1, TimerMode::Repeating),
-            bullet_offset: Vec3::new(0.0, 0.5, 0.6)
-        }).id()
+pub fn spawn_tower(commands: &mut Commands,
+                   meshes: &mut ResMut<Assets<Mesh>>,
+                   materials: &mut ResMut<Assets<StandardMaterial>>,
+                   position: Vec3,
+                   tower_type: TowerType) -> Entity {
+    info!("Spawning tower {:?}", tower_type);
+    let (color, tower) = tower_type.get_tower(materials);
+
+    commands.spawn(SpatialBundle::from_transform(Transform::from_translation(position)))
+        .insert(Name::new(format!("{:?}_Tower", tower_type)))
+        .insert(tower_type)
+        .insert(tower)
+        .with_children(|commands| {
+            commands.spawn(PbrBundle {
+                mesh: meshes.add(Cuboid::new(0.4, 1.5, 0.4)),
+                material: color,
+                transform: Transform::from_xyz(0.0, 0.0, 0.0),
+                ..default()
+            });
+        })
+        .id()
 }
--- src/dev/mod.rs
+++ src/dev/mod.rs
@@ -4,13 +4,17 @@ use bevy::prelude::*;
 use bevy_mod_picking::highlight::Highlight;
 use bevy_mod_picking::highlight::HighlightKind::Fixed;
 use bevy_mod_picking::PickableBundle;
+use bevy_mod_picking::prelude::NoDeselect;
+use rand::Rng;
 use crate::dev::dev_bullet::BulletPlugin;
 use crate::dev::dev_enemy::{BulletTowerPlugin};
 use crate::dev::dev_target::{Health, Target, TargetPlugin};
+use crate::dev::dev_tower_ui::DevTowerUiPlugin;
 
 pub mod dev_enemy;
 pub mod dev_target;
 mod dev_bullet;
+mod dev_tower_ui;
 
 #[derive(Component, Reflect, Default)]
 #[reflect(Component)]
@@ -23,6 +27,7 @@ impl Plugin for DebugPlugin {
         app.add_plugins(TargetPlugin)
             .add_plugins(BulletTowerPlugin)
             .add_plugins(BulletPlugin)
+            .add_plugins(DevTowerUiPlugin)
             .add_systems(Startup, generate_setup);
     }
 }
@@ -40,52 +45,8 @@ fn generate_setup(mut commands: Commands,
         .insert(Ground)
         .insert(Name::new("Ground"));
 
-    let default_color = materials.add(Color::srgba(0.3, 0.3, 0.3, 0.2));
-    let selected_color = materials.add(Color::srgba(0.8, 0.0, 0.3, 0.8));
-
     //Socket 01
-    commands.spawn(SpatialBundle::from_transform(Transform::from_xyz(0.0, 0.5, 0.0)))
-        .insert(Name::new("Socket-01"))
-        .insert(meshes.add(Capsule3d::new(0.2, 0.4)))
-        .insert(Highlight {
-            hovered: Some(Fixed(selected_color.clone())),
-            pressed: Some(Fixed(selected_color.clone())),
-            selected: Some(Fixed(selected_color.clone())),
-        })
-        .insert(default_color.clone())
-        .insert(NotShadowCaster)
-        .insert(PickableBundle::default())
-        .with_children(|commands| {
-            //Tower
-            commands.spawn(PbrBundle  {
-                mesh: meshes.add(Cuboid::new(0.5, 0.12, 0.5)),
-                material: materials.add(Color::srgb_u8(100, 100, 110)),
-                transform: Transform::from_xyz(0.0, -0.5, 0.0),
-                ..default()
-            });
-        });
-
-    //Socket02
-    commands.spawn(SpatialBundle::from_transform(Transform::from_xyz(10.0, 0.5, 0.0)))
-        .insert(Name::new("Socket-02"))
-        .insert(meshes.add(Capsule3d::new(0.2, 0.4)))
-        .insert(Highlight {
-            hovered: Some(Fixed(selected_color.clone())),
-            pressed: Some(Fixed(selected_color.clone())),
-            selected: Some(Fixed(selected_color.clone())),
-        })
-        .insert(default_color.clone())
-        .insert(NotShadowCaster)
-        .insert(PickableBundle::default())
-        .with_children(|commands| {
-            //Plate
-            commands.spawn(PbrBundle  {
-                mesh: meshes.add(Cuboid::new(0.5, 0.12, 0.5)),
-                material: materials.add(Color::srgb_u8(100, 100, 110)),
-                transform: Transform::from_xyz(0.0, -0.5, 0.0),
-                ..default()
-            });
-        });
+    spawn_random_plates(&mut commands, &mut meshes, &mut materials, 10);
 
     //Player (Placeholder)
     commands.spawn(PbrBundle {
@@ -107,4 +68,40 @@ fn generate_setup(mut commands: Commands,
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
+}
+
+fn spawn_random_plates(commands: &mut Commands,
+                       meshes: &mut ResMut<Assets<Mesh>>,
+                       materials: &mut ResMut<Assets<StandardMaterial>>, count: u8) {
+    let default_color = materials.add(Color::srgba(0.3, 0.3, 0.3, 0.2));
+    let selected_color = materials.add(Color::srgba(0.8, 0.0, 0.3, 0.8));
+    let mut rng = rand::thread_rng();
+    let max_value = 10.0;
+    let min_value = -10.0;
+    //Select Node
+    for _ in 0..count {
+        let x_rand = rng.gen_range(min_value..max_value);
+        let z_rand = rng.gen_range(min_value..max_value);
+        commands.spawn(SpatialBundle::from_transform(Transform::from_xyz(x_rand, 0.5, z_rand)))
+            .insert(Name::new(format!("Socket-{}", count)))
+            .insert(meshes.add(Capsule3d::new(0.2, 0.4)))
+            .insert(Highlight {
+                hovered: Some(Fixed(selected_color.clone())),
+                pressed: Some(Fixed(selected_color.clone())),
+                selected: Some(Fixed(selected_color.clone())),
+            })
+            .insert(default_color.clone())
+            .insert(NotShadowCaster)
+            .insert(PickableBundle::default())
+            .insert(NoDeselect)
+            .with_children(|commands| {
+                //Plate
+                commands.spawn(PbrBundle  {
+                    mesh: meshes.add(Cuboid::new(0.5, 0.12, 0.5)),
+                    material: materials.add(Color::srgb_u8(100, 100, 110)),
+                    transform: Transform::from_xyz(0.0, -0.5, 0.0),
+                    ..default()
+                });
+            });
+    }
 }
